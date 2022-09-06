af24aae79b2493210051f88d009ab810805bae8e
$~Bunni <55363475+ohemilyy@users.noreply.github.com>
2022-09-06 13:45:37 -0400

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ export default function NotFoundError() {
     return (
         <div className="px-12 flex flex-col gap-x-4 gap-y-8 items-center justify-center">
             <h1 className="font-AegixMono text-6xl font-bold text-center">404</h1>
-            <h1 className="font-AegixHeader text-center text-3xl font-bold">The page that you were looking for couldn't be found. The content that you are looking for may have since been moved or deleted from the site.</h1>
+            <h1 className="font-AegixHeader text-center text-3xl font-bold">The page that you were looking for could not be found. The content that you are looking for may have since been moved or deleted from the site.</h1>
             <Link href="/">
                 <a className={`
                     text-gray-100 hover:bg-slate-800 font-AegixHeader font-bold text-lg px-4 py-2 rounded-xl
