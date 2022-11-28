292d51ca01f77b59a2cc58755cfea441bdd9a487
OhEmilyy <itzbunniyt@gmail.com>
2022-11-28 12:58:36 -0500

    analytics from vercel

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Head from '/components/Root/Head';
 import Body from '/components/Root/Body';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { Analytics } from '@vercel/analytics/react';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 
@@ -25,6 +26,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head />
+      <Analytics />
       <Body>
         <Component {...pageProps} />  
       </Body>
